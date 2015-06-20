f268a958bd60f2c4308bc3f1a4b1a46b76347f81
Damián Silvani <munshkr@gmail.com>
2015-06-20 09:03:53 -0300

    Initial commit - bundle gem

---
+++ lib/wisper/sequel.rb
@@ -0,0 +1,7 @@
+require "wisper/sequel/version"
+
+module Wisper
+  module Sequel
+    # Your code goes here...
+  end
+end
---
+++ lib/wisper/sequel/version.rb
@@ -0,0 +1,5 @@
+module Wisper
+  module Sequel
+    VERSION = "0.1.0"
+  end
+end
